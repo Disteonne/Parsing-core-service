d1e79b846cd7cc37cc586726b97b3a051e1f35b9
Diesteonne <diana.razmikovna@yandex.ru>
2021-07-27 19:28:50 +0300

    Initial stage of the project

---
+++ src/main/java/com/disteonne/parsingcoreservice/ParsingCoreServiceApplication.java
@@ -0,0 +1,13 @@
+package com.disteonne.parsingcoreservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParsingCoreServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParsingCoreServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/disteonne/parsingcoreservice/ParsingCoreServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.disteonne.parsingcoreservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParsingCoreServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
